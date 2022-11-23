fd65d2b5a49ae61297f6a7554d790669d7445dfd
Caroline <caroline.syokau@student.moringaschool.com>
2022-11-23 11:40:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(r){
+// Write your code here*/
+
+if (r <=400) {
+  return 'This one is on me!';
+}
+else if(r>2500) {
+  return 'No can do.';
+}
+else if(r>2000) {
+  return 'I will gladly take your thirty bucks.';
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC"? 'Ok, sounds good.':'No go.';
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      
+    case "not as generous":
+      return "Thank you."
+
+    default:
+      return "Bye."
+      
+  }
 }
